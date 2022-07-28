92b997669c46b21f474e06d72227ad153214d940
yoonms0617 <yoonminwater@gmail.com>
2022-07-28 23:58:46 +0900

    chore: 프로젝트 생성

---
+++ backend/src/main/java/com/yoonmarket/YoonmarketApplication.java
@@ -0,0 +1,13 @@
+package com.yoonmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoonmarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoonmarketApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/yoonmarket/YoonmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoonmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoonmarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
